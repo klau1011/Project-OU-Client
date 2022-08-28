5feda1f44c858157f990a52d80822bdf773ffd8f
Kevin Lau <kevlau82@gmail.com>
2022-08-28 19:33:42 -0400

    touched up footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer() {
     <>
     <div className="big-container">
     
-    <p style={{color: '#737373'}}>Feel free to message me at @kxvxn.l on Instagram if you have any suggestions!</p>
+    <p className="thing" style={{color: '#737373'}}>Feel free to message me at <a href="https://www.instagram.com/kxvxn.l/" target="_blank">@kxvxn.l</a> on Instagram if you have any suggestions!</p>
     </div>
     </>
   )
